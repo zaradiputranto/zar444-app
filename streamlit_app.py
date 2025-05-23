efd51bf04ac21231836a86b675e6899e413fa7b0
zaradiputranto <nazarafaliya@gmail.com>
2025-05-23 09:09:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("nazara's page")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "hellooo"
 )
